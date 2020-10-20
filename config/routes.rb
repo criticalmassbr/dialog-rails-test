9a2041392389f3b0d10492e46ffddfade0a6f45c
firmo holanda <firmo@viize.com>
2020-10-20 10:23:37 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+
+  # home
+  root 'static_pages#home'
+
+  # user
   resources :users
+
+  # static_pages
   get 'static_pages/home'
   get 'static_pages/api'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
 end
