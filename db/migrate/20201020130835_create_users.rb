265ebd30bd8ade88b021202cbdbd42385d9bca0a
firmo holanda <firmo@viize.com>
2020-10-20 10:20:35 -0300

    add webpack and yard

---
+++ db/migrate/20201020130835_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :nome
+      t.string :idade
+      t.string :sexo
+      t.string :cidade
+      t.string :uf
+
+      t.timestamps
+    end
+  end
+end
