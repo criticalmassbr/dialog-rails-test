94c03306875bdd2194b94eb54ec154906d8a82e1
firmo holanda <firmo@viize.com>
2020-10-21 15:55:45 -0300

    remove old files

--- src/view/error.js
+++
@@ -1,5 +0,0 @@
-const error = `
-  <div class="alert alert-danger" id="displayError" role="alert" style="display: none"></div>
-`;
-
-export default error;
--- src/view/jumbotron.js
+++
@@ -1,8 +0,0 @@
-const jumbotron = `
-  <div class='jumbotron m-3 p-3 shadow-sm'>
-    <h1 class='display-4'>dialog-node</h1>
-    <p class='lead'>this is a simple weather-app-js-webpack app</p>
-  </div>
-  `;
-
-export default jumbotron;
