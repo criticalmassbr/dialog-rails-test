db0223ff2c70aad22cabe1d4dc906006974a94ed
firmo holanda <firmo@viize.com>
2020-10-21 13:48:33 -0300

    add error handling

--- src/index.js
+++ src/index.js
@@ -13,3 +13,14 @@ main.insertAdjacentHTML('beforeEnd', error);
 main.insertAdjacentHTML('beforeEnd', header);
 main.insertAdjacentHTML('beforeEnd', jumbotron);
 main.insertAdjacentHTML('beforeEnd', footer);
+
+// get html elements
+const displayErrorElement = document.getElementById('displayError');
+
+// display erro messages
+const displayError = (err) => {
+  displayErrorElement.innerHTML = err;
+  displayErrorElement.style.display = 'block';
+  setTimeout(() => { displayErrorElement.style.display = 'none'; }, 3000);
+  searchImput.value = '';
+};
