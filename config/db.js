bf5f732b41f8c2800a0ebdd456a92265ad322c1c
firmo holanda <firmo@viize.com>
2020-10-22 07:41:42 -0300

    add db config

---
+++ config/db.js
@@ -0,0 +1,9 @@
+require('dotenv').config({ path: '../.env' })
+
+module.exports = {
+  HOST: process.env.DB_HOST,
+  USER: process.env.DB_USER,
+  PASSWORD: process.env.DB_PASS,
+  DB: process.env.DB_NAME,
+  dialect: 'sqlite',
+};
