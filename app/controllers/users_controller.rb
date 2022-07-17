f82f657ca62a8afc292e7584844815ff242d18a4
Bruno <bruno_zangi@hotmail.com>
2022-07-17 09:36:35 -0300

    Add create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,5 +7,23 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to @user, notice: "User successfully created!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :age, :sex, :city, :uf)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    validates :name, presence: { message: "name must be filled" }
+    validates :age, presence: true, numericality: {only_integer: true, greater_than: 0}
+    validates :sex, inclusion: { in: %w[Male Female Other].freeze }
+    validates :city, presence: { message: "city must be filled" }
+    validates :uf, inclusion: { in: ApplicationHelper::UF }
 end
