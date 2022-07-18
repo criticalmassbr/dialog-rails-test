6cd1af49934a63d06555398f93d385fa3d0c38af
Bruno <bruno_zangi@hotmail.com>
2022-07-18 00:47:35 -0300

    Add update user option

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,20 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(@user_params)
+      redirect_to @user, notice: "User successfully updated!"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+  
   private
 
   def user_params
